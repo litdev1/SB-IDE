1319ee0dec0cbc090d2bed76d16bd6d04d8fa133
steve todman <steve.todman@live.co.uk>
2017-10-29 23:16:24 +0000

    1.0.3

--- SB-IDE/SBInterop.cs
+++ SB-IDE/SBInterop.cs
@@ -13,7 +13,7 @@ namespace SB_IDE
 {
     public class SBInterop
     {
-        int currentVersion = 2;
+        int currentVersion = 3;
 
         object Service = null;
         MethodInfo SaveProgram = null;
