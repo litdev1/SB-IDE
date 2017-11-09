162d9aae59182f1b11c2d81580b922bdbf06abf3
steve todman <steve.todman@live.co.uk>
2017-11-09 22:10:48 +0000

    font and color context menu

--- SB-IDE/SBContext.cs
+++ SB-IDE/SBContext.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace SB_IDE
 {
@@ -12,11 +14,16 @@ namespace SB_IDE
     {
         private SBDocument sbDocument;
         private Scintilla textArea;
+        private ToolStripMenuItem menuColors;
+        private ToolStripMenuItem menuFonts;
 
         public SBContext(SBDocument sbDocument)
         {
             this.sbDocument = sbDocument;
             textArea = sbDocument.TextArea;
+
+            menuColors = SetColors();
+            menuFonts = SetFonts();
         }
 
         public void SetMenu()
@@ -39,6 +46,67 @@ namespace SB_IDE
             menu.Items.Add(new ToolStripSeparator());
             menu.Items.Add(new ToolStripMenuItem("Format Program", null, (s, ea) => sbDocument.Lexer.Format()));
             menu.Items.Add(new ToolStripMenuItem("Add to Debug Watch Ctrl+W", null, (s, ea) => sbDocument.AddWatch()) { Enabled = textArea.SelectedText.Length > 0 });
+            menu.Items.Add(new ToolStripSeparator());
+            menu.Items.Add(menuColors);
+            menu.Items.Add(menuFonts);
+        }
+
+        private void Insert(object sender, EventArgs e)
+        {
+            ToolStripMenuItem menuItem = (ToolStripMenuItem)sender;
+            textArea.InsertText(textArea.CurrentPosition, "\"" + menuItem.Text + "\"");
+            textArea.CurrentPosition += 2 + menuItem.Text.Length;
+            textArea.ClearSelections();
+        }
+
+        private ToolStripMenuItem SetColors()
+        {
+            Array colorsArray = Enum.GetValues(typeof(KnownColor));
+            KnownColor[] allColors = new KnownColor[colorsArray.Length];
+            Array.Copy(colorsArray, allColors, colorsArray.Length);
+
+            ToolStripMenuItem menuItem = new ToolStripMenuItem("Insert Color");
+
+            for (int i = 0; i < allColors.Length; i++)
+            {
+                if (allColors[i] < KnownColor.Transparent || allColors[i] >= KnownColor.ButtonFace) continue;
+                String name = allColors[i].ToString();
+                Bitmap bmp = new Bitmap(50, 50);
+                Graphics g = Graphics.FromImage(bmp);
+                g.Clear(Color.FromName(allColors[i].ToString()));
+                menuItem.DropDownItems.Add(new ToolStripMenuItem(name, bmp, Insert));
+            }
+
+            return menuItem;
+        }
+
+        private ToolStripMenuItem SetFonts()
+        {
+            ToolStripMenuItem menuItem = new ToolStripMenuItem("Insert Font");
+
+            int i = 0;
+            foreach (FontFamily font in FontFamily.Families)
+            {
+                //if (font.Name == "Cambria Math") continue;
+                //if (font.Name == "Gabriola") continue;
+                //if (font.Name == "Jokerman") continue;
+
+                int size = 24;
+                Bitmap bmp = new Bitmap(4*size, 3*size/2);
+                Graphics g = Graphics.FromImage(bmp);
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                g.DrawString("Basic", new Font(font, size, FontStyle.Regular, GraphicsUnit.Pixel), Brushes.Black, 1, 1);
+
+                ToolStripMenuItem item = new ToolStripMenuItem(font.Name, bmp, Insert);
+                item.ImageScaling = ToolStripItemImageScaling.None;
+                //item.Font = new Font(item.Font.FontFamily, 24, FontStyle.Regular, GraphicsUnit.Point);
+                menuItem.DropDownItems.Add(item);
+                i++;
+            }
+
+            return menuItem;
         }
     }
 }
