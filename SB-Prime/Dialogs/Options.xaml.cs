6f59313e5ffa03f6f4ea65d70fccfed610eba66f
litdev1 <steve.todman@live.co.uk>
2024-04-26 21:45:18 +0100

    minor installdir change logic on options OK

--- SB-Prime/Dialogs/Options.xaml.cs
+++ SB-Prime/Dialogs/Options.xaml.cs
@@ -115,9 +115,11 @@ namespace SB_Prime.Dialogs
                 InstallDir = comboBoxInstallation.SelectedItem.ToString();
             }
             if (!Directory.Exists(InstallDir)) InstallDir = "";
-            bool newInstallDir = MainWindow.InstallDir != InstallDir;
-            MainWindow.InstallDir = InstallDir;
-            if (newInstallDir) mainWindow.sbInterop = new SBInterop();
+            if (MainWindow.InstallDir != InstallDir)
+            {
+                MainWindow.InstallDir = InstallDir;
+                mainWindow.sbInterop = new SBInterop();
+            }
             MainWindow.InstallDirExtra.Clear();
             foreach (var installDir in comboBoxInstallation.Items)
             {
