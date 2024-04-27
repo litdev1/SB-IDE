ebc5193268131dccce5a74bf552ed68910d53045
litdev1 <steve.todman@live.co.uk>
2024-04-27 11:16:35 +0100

    ParseLineVariables default true

--- SB-Prime/Properties/Settings.Designer.cs
+++ SB-Prime/Properties/Settings.Designer.cs
@@ -500,7 +500,7 @@ namespace SB_Prime.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool ParseLineVariables {
             get {
                 return ((bool)(this["ParseLineVariables"]));
